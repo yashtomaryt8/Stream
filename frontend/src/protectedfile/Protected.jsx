342c8bee30c54c4b6117e417d7aa81eef0c2b64e
Yash Tomar <yashtomar.yt8@gmail.com>
2025-09-02 19:45:53 +0530

    deploy ready

--- frontend/src/protectedfile/Protected.jsx
+++ frontend/src/protectedfile/Protected.jsx
@@ -1,22 +1,23 @@
-//we create this because we want this component use in each home,search,and upload so we just wrap them with protected in the app.routes.jsx
-
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Protected = ({ children }) => {
-    const navigate= useNavigate();
-    useEffect(() => {
-        axios.get("http://localhost:3000/auth/me", {
-            withCredentials: true
-        }).then(response => {
-            console.log(response.data);
-        }).catch(()=>{
-            navigate("/login");
-        
-        })
-}, [])
-return children;
-}
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/me`, {
+      withCredentials: true,
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(() => {
+      navigate("/login");
+    });
+  }, []);
+
+  return children;
+};
 
 export default Protected;
