0072eae1753773060d3ab7e9c6d2fd5506356186
Yash Tomar <yashtomar.yt8@gmail.com>
2025-09-03 00:49:24 +0530

    check2

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,16 +1,15 @@
 import { log } from 'console';
 import mongoose from 'mongoose';
 
-function connectToDatabase(){
-
-    mongoose.connect("mongodb://localhost:27017/n22-music-project").then(() => {
-
-        console.log("Connected to the database successfully");
-        
-    }).catch((error) => {
-        log("Error connecting to the database:", error);
-    })
+const connectToDatabase = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log("MongoDB connected successfully"))
+        await mongoose.connect(`${process.env.MONGODB_URI}/steam`)
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1); // Exit the process with failure
+    }
 }
 
-
 export default connectToDatabase;
+
