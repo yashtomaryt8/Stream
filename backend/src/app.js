db5f24609a83a035f5ca6697f66fd73a2184318f
Yash Tomar <yashtomar.yt8@gmail.com>
2025-10-06 00:53:38 +0530

    cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",          // local frontend
-    // "https://stream-sandy-eight.vercel.app"  // deployed frontend
+    "https://steam-henna.vercel.app"  // deployed frontend
   ],
   credentials: true
 }));
