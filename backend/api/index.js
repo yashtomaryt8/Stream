86d1384f42953e1663046a5d61e687d3d84e4686
Yash Tomar <yashtomar.yt8@gmail.com>
2025-10-05 20:23:53 +0530

    t1

--- backend/api/index.js
+++
@@ -1,7 +0,0 @@
-import app from "../backend/src/app.js";
-import connectToDatabase from "../backend/src/db/db.js";
-import serverless from "serverless-http";
-
-connectToDatabase();
-
-export default serverless(app);
